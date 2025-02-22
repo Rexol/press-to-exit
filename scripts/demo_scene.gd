5253037e1af97b62356efa4b59884dd56754e568
Vladimir Surtaev <surtaevv@inbox.ru>
2025-02-22 16:04:08 +0200

    Add controls hints
    test

--- scripts/demo_scene.gd
+++ scripts/demo_scene.gd
@@ -2,12 +2,25 @@ extends Node3D
 
 
 @onready var slot_door = $Walls/SlotDoor
+@onready var control_hint = $CanvasLayer/BasicControls
+@onready var slot_hint = $CanvasLayer/SlotControls
+@onready var try_again_hint = $CanvasLayer/TryAgainHint
 
 var slot_available = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	control_hint.visible = true  # Show at the start
+
+func _input(event):
+	if event.is_action_released("latter_button"):
+		hide_message(control_hint)
+
+func hide_message(label: Label):
+	var tween = create_tween()
+	tween.tween_property(label, "modulate:a", 0, 1.0)  # Fade out in 1 sec
+	await tween.finished
+	label.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -26,5 +39,8 @@ func _on_level_end_trigger_body_entered(body:Node3D) -> void:
 func _on_open_slot_door_body_entered(body:Node3D) -> void:
 	if body.is_in_group("cube"):
 		if slot_available:
+			try_again_hint.visible = true
+			await get_tree().create_timer(1.5).timeout
 			slot_available = false
+			hide_message(try_again_hint)
 			slot_door.free()
